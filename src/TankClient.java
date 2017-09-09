fb463061dd80f42f55873de123a708559669482f
1298630983 <1298630983@qq.com>
2017-09-09 11:33:29 +0800

    TankWar0.3

--- src/TankClient.java
+++ src/TankClient.java
@@ -6,12 +6,15 @@ import java.awt.event.WindowEvent;
  * Created by Jason on 2017/9/8.
  */
 public class TankClient extends Frame{
+    int x = 50 , y = 50;
+    Image offScreenImage = null;
+
     public static void main(String[] args) {
         new TankClient().lanuchFrame();
     }
 
     public void lanuchFrame() {
-        setBounds(100,100,800,600);
+        setBounds(x,y,800,600);
         setResizable(false);
         setTitle("TankWar");
         setBackground(Color.green);
@@ -22,13 +25,45 @@ public class TankClient extends Frame{
             }
         });
         setVisible(true);
+        new Thread(new PaintThread()).start();
     }
 
     @Override
     public void paint(Graphics g) {
         Color c = g.getColor();
         g.setColor(Color.red);
-        g.fillOval(50,50,30,30);
+        g.fillOval(x,y,30,30);
         g.setColor(c);
+
+        y += 5;
+    }
+
+    @Override
+    public void update(Graphics g) {
+        if (offScreenImage == null) {
+            offScreenImage = this.createImage(800,600);
+        }
+        Graphics gOffScreen = offScreenImage.getGraphics();
+        Color c = gOffScreen.getColor();
+        gOffScreen.setColor(Color.green);
+        gOffScreen.fillRect(0,0,800,600);
+        gOffScreen.setColor(c);
+        paint(gOffScreen);
+        g.drawImage(offScreenImage,0,0,null);
+    }
+
+    private class PaintThread implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                repaint();
+                try {
+                    Thread.sleep(150);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
