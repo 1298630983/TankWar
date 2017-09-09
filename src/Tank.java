37ae577abe881c39a2e1ae668e48fcb8a47b9a0d
1298630983 <1298630983@qq.com>
2017-09-09 15:42:50 +0800

    TankWar0.7

--- src/Tank.java
+++ src/Tank.java
@@ -5,7 +5,15 @@ import java.awt.event.KeyEvent;
  * Created by Jason on 2017/9/9.
  */
 public class Tank {
-    int x, y;
+    public static final int XSPEED = 5;
+    public static final int YSPEED = 5;
+
+    private int x, y;
+    private boolean bL = false, bU = false, bR = false, bD = false;
+
+    enum Direction {L, LU, U, UR, R, RD, D, LD, STOP };
+
+    private Direction dir = Direction.STOP;
 
     public Tank(int x, int y) {
         this.x = x;
@@ -17,19 +25,92 @@ public class Tank {
         g.setColor(Color.red);
         g.fillOval(x,y,30,30);
         g.setColor(c);
+
+        move();
+    }
+
+    public void move() {
+        switch (dir) {
+            case L:
+                x -= XSPEED;
+                break;
+            case LU:
+                x -= XSPEED;
+                y -= YSPEED;
+                break;
+            case U:
+                y -= YSPEED;
+                break;
+            case UR:
+                x += XSPEED;
+                y -= YSPEED;
+                break;
+            case R:
+                x += XSPEED;
+                break;
+            case RD:
+                x += XSPEED;
+                y += YSPEED;
+                break;
+            case D:
+                y += YSPEED;
+                break;
+            case LD:
+                x -= XSPEED;
+                y += YSPEED;
+                break;
+            case STOP:
+                break;
+        }
     }
 
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
         switch (key) {
             case KeyEvent.VK_LEFT :
-                x -= 5;break;
+                bL = true;
+                break;
             case KeyEvent.VK_RIGHT :
-                x += 5;break;
+                bR = true;
+                break;
             case KeyEvent.VK_UP :
-                y -= 5;break;
+                bU = true;
+                break;
             case KeyEvent.VK_DOWN :
-                y += 5;break;
+                bD = true;
+                break;
         }
+        locateDirection();
+    }
+
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+        switch (key) {
+            case KeyEvent.VK_LEFT:
+                bL = false;
+                break;
+            case KeyEvent.VK_RIGHT:
+                bR = false;
+                break;
+            case KeyEvent.VK_UP:
+                bU = false;
+                break;
+            case KeyEvent.VK_DOWN:
+                bD = false;
+                break;
+        }
+        locateDirection();
+    }
+
+    public void locateDirection() {
+        if (bL && !bU && !bR && !bD) {dir = Direction.L;}
+        else if (bL && bU && !bR && !bD) {dir = Direction.LU;}
+        else if (!bL && bU && !bR && !bD) {dir = Direction.U;}
+        else if (!bL && bU && bR && !bD) {dir = Direction.UR;}
+        else if (!bL && !bU && bR && !bD) {dir = Direction.R;}
+        else if (!bL && !bU && bR && bD) {dir = Direction.RD;}
+        else if (!bL && !bU && !bR && bD) {dir = Direction.D;}
+        else if (bL && !bU && !bR && bD) {dir = Direction.LD;}
+        else if (!bL && !bU && !bR && !bD) {dir = Direction.STOP;}
     }
 }
--- src/TankClient.java
+++ src/TankClient.java
@@ -73,5 +73,10 @@ public class TankClient extends Frame{
         public void keyPressed(KeyEvent e) {
             myTank.keyPressed(e);
         }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            myTank.keyReleased(e);
+        }
     }
 }
